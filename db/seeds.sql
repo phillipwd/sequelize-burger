69f0579444fd3acbf29d20cbb2662e0b3f1d2ef0
Phillip Dunham <phillipwwd@gmail.com>
2020-01-21 15:04:15 -0800

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bamburgurber", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Breakfast Burger", true);
+INSERT INTO burgers (burger_name) VALUES ("Ostrich Burger");
+INSERT INTO burgers (burger_name) VALUES ("Unknown Error Burger");
