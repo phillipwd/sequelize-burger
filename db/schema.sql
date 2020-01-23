c254d0a2eb712eb6073212b607db44b392e62527
Phillip Dunham <phillipwwd@gmail.com>
2020-01-23 09:30:26 -0800

    Changed schema to include SET SQL_MODE change.

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,7 @@ DROP DATABASE IF EXISTS burgerseq_db;
 CREATE DATABASE burgerseq_db;
 
 USE burgerseq_db;
+SET SQL_MODE = ' ';
 CREATE TABLE burgers(
     id INTEGER NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR (255),
