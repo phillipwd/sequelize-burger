ba8149e6766e77599bcffde90973781176392224
Phillip Dunham <phillipwwd@gmail.com>
2020-01-29 14:40:32 -0800

    Removing commented orm code.

--- models/burger.js
+++ models/burger.js
@@ -1,27 +1,3 @@
-// var burger = {
-//     all: function(cb) {
-//       orm.all("burgers", function(res) {
-//         cb(res);
-//       });
-//     },
-
-//     create: function(cols, vals, cb) {
-//       orm.create("burgers", cols, vals, function(res) {
-//         cb(res);
-//       });
-//     },
-//     update: function(objColVals, condition, cb) {
-//       orm.update("burgers", objColVals, condition, function(res) {
-//         cb(res);
-//       });
-//     },
-//     delete: function(condition, cb) {
-//       orm.delete("burgers", condition, function(res) {
-//         cb(res);
-//       });
-//     }
-//   };
-  
   module.exports = function(sequelize, DataTypes){
     var Burger = sequelize.define("Burger",{
       burger_name : {
